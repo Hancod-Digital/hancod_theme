233cb42dfc9075dcb6ab5d0add656cb84cab8ef3
sufiyanhancod <sufiyan@hancod.com>
2025-02-26 09:42:33 +0530

    added colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,11 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFF0A003D);
+  static const Color primaryColor = Color(0xFFEC1E26);
   static const Color white = Color(0xFFFFFFFF);
-  static const stormyBlue = Color(0xFF75829C);
+  static const Color stormyBlue = Color(0xFF75829C);
+  static const Color secondaryColor = Color(0xFFABAAB1);
+  static const Color secondaryBackgroundColor = Color(0xFFF8F8F8);
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFF8C8C8C);
 }
