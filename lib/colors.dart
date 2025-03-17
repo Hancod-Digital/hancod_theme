6a27a7e7322bce6d804c20980899c279f0d6d6bb
Alfas Mecheeri <devalfaz@outlook.com>
2025-03-17 17:30:37 +0530

    changed color

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFFEC1E26);
+  static const Color primaryColor = Color(0xFF0A003D);
   static const Color white = Color(0xFFFFFFFF);
   static const Color stormyBlue = Color(0xFF75829C);
   static const Color title = Color(0xFF2E2E2E);
