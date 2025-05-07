e50a2ab8650b7795320a870339254df793f018ca
Alfas Mecheeri <devalfaz@outlook.com>
2025-05-08 01:10:31 +0530

    fix in typeahead

--- lib/forms/app_typeahead_form.dart
+++ lib/forms/app_typeahead_form.dart
@@ -46,10 +46,23 @@ class AppTypeAheadForm<T> extends AppForm<T> {
 
 class _AppTypeAheadFormState<T> extends State<AppTypeAheadForm<T>> {
   late GlobalKey<FormBuilderFieldState> key;
+  late TextEditingController controller;
   @override
   void initState() {
     super.initState();
     key = widget.fieldKey ?? GlobalKey<FormBuilderFieldState>();
+    controller = widget.controller ?? TextEditingController();
+    final initialValue = widget.initialValue == null
+        ? null
+        : widget.initialValue is String
+            ? widget.initialValue
+            : widget.selectionToTextTransformer?.call(
+                widget.initialValue as T,
+              );
+    // If the initial value is not null, set the controller's text to the initial value
+    if (initialValue != null) {
+      controller.text = initialValue;
+    }
   }
 
   @override
