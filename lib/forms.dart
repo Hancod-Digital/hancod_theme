a1b2c470c7fc5eccd3a277a7c9afd9495ff63381
Alfas Mecheeri <devalfaz@outlook.com>
2025-06-11 13:47:36 +0530

    fix in forms

--- lib/forms.dart
+++ lib/forms.dart
@@ -5,7 +5,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart'
     hide DropdownButton, DropdownButtonFormField, DropdownButtonHideUnderline, DropdownMenuItem;
-import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
@@ -13,7 +12,7 @@ import 'package:form_builder_extra_fields/form_builder_extra_fields.dart';
 import 'package:hancod_theme/hancod_theme.dart';
 import 'package:multi_dropdown/multi_dropdown.dart';
 import 'package:phone_form_field/phone_form_field.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' hide TextDirection;
 
 part 'forms/app_checkbox_form.dart';
 part 'forms/app_datetime_form.dart';
