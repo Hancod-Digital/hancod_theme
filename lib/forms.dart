6914a62127b85a267bd6552b5754d497bfbf5f5e
Alfas Mecheeri <159182330+alfas-hancod@users.noreply.github.com>
2025-06-11 13:32:54 +0530

    Update forms.dart

--- lib/forms.dart
+++ lib/forms.dart
@@ -16,6 +16,7 @@ import 'package:form_builder_extra_fields/form_builder_extra_fields.dart';
 import 'package:hancod_theme/hancod_theme.dart';
 import 'package:multi_dropdown/multi_dropdown.dart';
 import 'package:phone_form_field/phone_form_field.dart';
+import 'package:intl/intl.dart';
 
 part 'forms/app_checkbox_form.dart';
 part 'forms/app_datetime_form.dart';
